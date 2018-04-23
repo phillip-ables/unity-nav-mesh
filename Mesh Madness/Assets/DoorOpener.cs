8f21a3ed82f511de68e8311c8f4e10bb6bba2251
lip ables <phillipables1@gmail.com>
2018-04-22 23:31:53 -0400

    OpenDoor bool to remember open status, on update if open lower door, else keep in 0 position, finaly open equals not open

--- Mesh Madness/Assets/DoorOpener.cs
+++ Mesh Madness/Assets/DoorOpener.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DoorOpener : MonoBehaviour {
+    bool open = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            if (open)
+                this.transform.Translate(0, -3, 0);
+            else
+                this.transform.Translate(0, 3, 0);
+
+            open = !open;
+        }
+    }
 }
